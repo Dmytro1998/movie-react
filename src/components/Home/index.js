967380a58c6feb26af3515baa8a36818e2803fcd
Dmytro <evdokimov041998@gmail.com>
2021-11-02 19:51:21 +0200

    home-page

---
+++ src/components/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../header/index.css"
+
+export class Home extends React.Component {
+    render() {
+      return (
+     <div className= "home-page">
+         home
+
+
+     </div>
+      
+     
+      );
+    }
+  }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../header/index.css"
+import { Home} from "../Home";
 import { BrowserRouter,  Route, Switch, NavLink } from 'react-router-dom';
 
 
@@ -10,11 +11,17 @@ export class Header extends React.Component {
         <BrowserRouter>
          <div className= "logo"><p>APJ</p><p>Furniture</p></div>
           <nav>
-            <li><NavLink to="/home" activeClassName ="selected" className ="navigation-item">Home</NavLink></li>
-            <li><NavLink to="/prodact" activeClassName ="selected" className ="navigation-item">Prodact</NavLink></li>
-            <li><NavLink to="/about" activeClassName ="selected"className ="navigation-item">About</NavLink></li>
-            <li><NavLink to="/contact" activeClassName ="selected"className ="navigation-item">Contact</NavLink></li>
+            <li><NavLink to="/home" activeClassName ="selected" className ="navigation-item-main">Home</NavLink></li>
+            <li><NavLink to="/prodact" activeClassName ="selected" className ="navigation-item-rest">Prodact</NavLink></li>
+            <li><NavLink to="/about" activeClassName ="selected"className ="navigation-item-rest">About</NavLink></li>
+            <li><NavLink to="/contact" activeClassName ="selected"className ="navigation-item-rest">Contact</NavLink></li>
          </nav>
+         <Switch>
+          <Route path="/home">
+            <Home/>
+           </Route> 
+        
+        </Switch> 
          </BrowserRouter>
         </div>
      
